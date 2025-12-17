c6d07596d59ebe833fd67bff95a78493ad091686
hajarsandid <hajarsandid.allobaba@gmail.com>
2025-12-17 23:03:10 +0100

    add explicit routes for HTML pages

--- app.js
+++ app.js
@@ -49,6 +49,21 @@ const initUsersTable = () => {
 
 initUsersTable();
 
+// Serve HTML pages
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 // Auth routes
 app.use('/auth', authRoutes);
 
